6a07b007c774486c63138575c17fb3743fe7f1cc
Serena Lertkantitham <serena.lertkantitham@gmail.com>
2019-08-02 08:09:58 -0700

    Updates

---
+++ precache-manifest.bdca673220548840fb819ae6a64d35a6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7b7bfe746f2ae4b858abf157363906",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "4ecae2eced5fec89fea7",
+    "url": "/my-portfolio/static/css/main.8341a253.chunk.css"
+  },
+  {
+    "revision": "bbba26c028be111a0988",
+    "url": "/my-portfolio/static/js/2.0b3a6241.chunk.js"
+  },
+  {
+    "revision": "4ecae2eced5fec89fea7",
+    "url": "/my-portfolio/static/js/main.6bfad71f.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "eb884f6af49bdc5057760a2700859567",
+    "url": "/my-portfolio/static/media/1hike2beer-mockup.eb884f6a.png"
+  },
+  {
+    "revision": "0418cedfb834a6c968b84cb513d1eccc",
+    "url": "/my-portfolio/static/media/SerenaLertkantitham.0418cedf.jpg"
+  },
+  {
+    "revision": "1dc7bc1eb3a715275af8c854bd3e955a",
+    "url": "/my-portfolio/static/media/beer.1dc7bc1e.gif"
+  },
+  {
+    "revision": "9d65743ef63df79af89008fd424a1274",
+    "url": "/my-portfolio/static/media/flex-mockup.9d65743e.png"
+  },
+  {
+    "revision": "2eefdf8848518f280d896b233ba5a06c",
+    "url": "/my-portfolio/static/media/hand_wave.2eefdf88.gif"
+  },
+  {
+    "revision": "5cb35bf3ed81088c5b240f0e602ef9a0",
+    "url": "/my-portfolio/static/media/online.5cb35bf3.gif"
+  },
+  {
+    "revision": "15e39e75c5fe7cccc12c7b9046b73731",
+    "url": "/my-portfolio/static/media/pencil.15e39e75.gif"
+  },
+  {
+    "revision": "00e4626dd7872a22f80dc0058cc89130",
+    "url": "/my-portfolio/static/media/resume_Lertkantitham.00e4626d.pdf"
+  },
+  {
+    "revision": "aee3d911d9cd1606f0c3709b98ec105e",
+    "url": "/my-portfolio/static/media/sunshine.aee3d911.gif"
+  },
+  {
+    "revision": "fe35f9d9e89cd0a434638f6ac41b2478",
+    "url": "/my-portfolio/static/media/weekly-mockup.fe35f9d9.png"
+  },
+  {
+    "revision": "2c947b2671ee58254638e4c95dd6e2a3",
+    "url": "/my-portfolio/static/media/yoga.2c947b26.gif"
+  }
+]);
